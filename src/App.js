73fa11d1840d0a69cc71c77b6d39cc0145e37183
cxg <gtcxg@hotmail.com>
2019-06-16 16:53:10 +0800

    add login page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import logo from './logo.svg';
 import MediaControlCard from './components/CardDemo'
-
+import LoginPage from './components/Login'
 
 class App extends Component {
 
