3f1cd4066d1f28f78ac0226a78aec60796ee41c3
cxg <gtcxg@hotmail.com>
2019-06-23 21:42:37 +0800

    get note for fret

--- src/components/SongPage.jsx
+++ src/components/SongPage.jsx
@@ -4,6 +4,7 @@ import Vex from 'vexflow';
 const {
   Accidental,
   Formatter,
+  TabStave,
   Stave,
   StaveNote,
   Renderer,
@@ -11,6 +12,8 @@ const {
   TabNote,
   Bend,
   Vibrato,
+  Tuning,
+  keyProperties,
 } = Vex.Flow;
 
 
@@ -26,7 +29,7 @@ export default class SongPage extends Component {
     const renderer = new Renderer(container, Renderer.Backends.SVG);
     renderer.resize(500, 500);
     const ctx = renderer.getContext();
-    const stave = new Stave(10, 40, 400);
+    const stave = new TabStave(10, 40, 400);
     stave.addClef("treble").addTimeSignature("4/4");
     stave.setContext(ctx).draw();
 
@@ -77,17 +80,39 @@ export default class SongPage extends Component {
       new Voice({num_beats: 4, beat_value: 4}).addTickables(notes2),
     ];
 
-    var fromatter = new Formatter().joinVoices(voices).format(voices, 400);
+    var canon_notes = [
+      new TabNote({
+        positions: [{str: 1, fret: 0}, {str: 2, fret: 1}, {str: 3, fret: 0}, {str: 5, fret: 3}],
+        duration: "h"
+      }),
+      /* new StaveNote({clef: "treble", keys: ["c/4", "c/5"], duration: "h"}),*/
+      new StaveNote({clef: "treble", keys: ["a#/3", "e/4", "g/4", "a/4"], duration: "h"}),
+    ];
+
+    var canon = [
+      new Voice({num_beats: 4, beat_value: 4}).addTickables(canon_notes)
+    ];
+
+    /* var fromatter = new Formatter().joinVoices(voices).format(voices, 400);*/
+    var fromatter = new Formatter().joinVoices(canon).format(canon, 400);
     // render voices
     /* voices.forEach(function (v) { v.draw(ctx, stave) });
      * Formatter.FormatAndDraw(ctx, stave, notes3);*/
-    Formatter.FormatAndDraw(ctx, stave, notes4);
+    Formatter.FormatAndDraw(ctx, stave, canon_notes);
+
+    var tuning = new Tuning();
+    var spec = tuning.getNosteForFret(1, 1);
+    console.log("spec", spec);
+    var spec_props = keyProperties(spec);
+    console.log("spec_props", spec_props);
+
 
     this.refs.outer.appendChild(container);
   }
 
 
   render() {
+
     return (
       <div ref="outer"></div>
     )
